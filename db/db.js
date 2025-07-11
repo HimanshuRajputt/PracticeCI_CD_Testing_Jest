408e7f06bb11a53f3ba6c395420b45affc05cec6
Himanshu rajputt <rajput6600himanshu@gmail.com>
2025-07-11 15:42:42 +0530

    update docker image build and push in Hub-2

--- db/db.js
+++ db/db.js
@@ -6,8 +6,8 @@ dotenv.config()
 export const ConnectDB= async ()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        console.log("Connected to DB 👍")
+        console.log("Connected to DB ")
     } catch (error) {
-        console.log("Not able to Connect DB ✖️")
+        console.log("Not able to Connect DB ")
     }
 }
